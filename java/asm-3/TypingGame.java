fe230ec38d53eef1a7c729b78568896efd648df4
tszhong0411 <me@honghong.me>
2025-04-25 09:31:03 +0800

    refactor(java-asm-3): reorganize key event handling and UI component creation

--- java/asm-3/TypingGame.java
+++ java/asm-3/TypingGame.java
@@ -38,6 +38,44 @@ public class TypingGame implements KeyListener {
       "LAI", "COMPUTER", "PROGRAMMING", "CAT", "OBJECT", "COLLEGE", "CLASS", "TEXT", "COMMUNITY", "AI"
   };
 
+  public TypingGame() {
+    frame = new JFrame("Typing Game");
+    JPanel infoPanel = createInfoPanel();
+    JPanel mainPanel = createMainPanel();
+    JPanel keyboardPanel = createKeyboardPanel();
+
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(640, 400);
+    frame.addKeyListener(this);
+
+    frame.addWindowListener(new WindowAdapter() {
+      @Override
+      public void windowActivated(WindowEvent e) {
+        frame.requestFocusInWindow();
+      }
+    });
+
+    frame.setFocusable(true);
+    frame.requestFocusInWindow();
+    frame.setVisible(true);
+
+    frame.add(infoPanel, BorderLayout.NORTH);
+    frame.add(mainPanel, BorderLayout.CENTER);
+    frame.add(keyboardPanel, BorderLayout.SOUTH);
+
+    newRound();
+
+    // Timer
+    timer = new Timer(1000, new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        counter++;
+        setTimeLabel(counter);
+      }
+    });
+    timer.start();
+  }
+
   private JLabel createInfoLabel(String text) {
     JLabel label = new JLabel(text, SwingConstants.CENTER);
     label.setFont(new Font("Arial", Font.PLAIN, 20));
@@ -79,83 +117,25 @@ public class TypingGame implements KeyListener {
       button.setFont(new Font("Arial", Font.PLAIN, 24));
       keyboardPanel.add(button);
       keyButtons.put(key, button);
+
+      // Add mouse listener to simulate keyPressed and keyReleased
+      button.addMouseListener(new MouseAdapter() {
+        @Override
+        public void mousePressed(MouseEvent e) {
+          handleKeyPressed(key.charAt(0));
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+          handleKeyReleased(key.charAt(0));
+        }
+      });
     }
 
     return keyboardPanel;
   }
 
-  public TypingGame() {
-    frame = new JFrame("Typing Game");
-    JPanel infoPanel = createInfoPanel();
-    JPanel mainPanel = createMainPanel();
-    JPanel keyboardPanel = createKeyboardPanel();
-
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setSize(640, 400);
-    frame.addKeyListener(this);
-
-    frame.addWindowListener(new WindowAdapter() {
-      @Override
-      public void windowActivated(WindowEvent e) {
-        frame.requestFocusInWindow();
-      }
-    });
-
-    frame.setFocusable(true);
-    frame.requestFocusInWindow();
-    frame.setVisible(true);
-
-    frame.add(infoPanel, BorderLayout.NORTH);
-    frame.add(mainPanel, BorderLayout.CENTER);
-    frame.add(keyboardPanel, BorderLayout.SOUTH);
-
-    newRound();
-
-    // Timer
-    timer = new Timer(1000, new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        counter++;
-        setTimeLabel(counter);
-      }
-    });
-    timer.start();
-  }
-
-  private void setTimeLabel(int time) {
-    timeLabel.setText("Time: " + time + "s");
-  }
-
-  private void newRound() {
-    roundCount++;
-    roundLabel.setText("Round: " + roundCount);
-
-    int randomIndex = (int) (Math.random() * WORDS.length);
-    originalWord = WORDS[randomIndex];
-    currentWord = originalWord;
-    wordLabel.setText(currentWord);
-    wordLabel.setBackground(DEFAULT_MAIN_PANEL_COLOR);
-
-    frame.requestFocusInWindow();
-  }
-
-  private void endGame() {
-    isGameOver = true;
-    timer.stop();
-    // Use HTML to create a new line
-    wordLabel
-        .setText("<html><div style='text-align: center;font-size: 16px;'>Game End!<br>Time used: " + counter + " second"
-            + (counter > 1 ? "s" : "") + "</div></html>");
-  }
-
-  @Override
-  public void keyTyped(KeyEvent e) {
-    // Nothing to do here
-  }
-
-  @Override
-  public void keyReleased(KeyEvent e) {
-    char released = Character.toUpperCase(e.getKeyChar());
+  private void handleKeyReleased(char released) {
     String keyStr = String.valueOf(released);
     JButton releasedButton = keyButtons.get(keyStr);
     if (releasedButton != null) {
@@ -163,9 +143,7 @@ public class TypingGame implements KeyListener {
     }
   }
 
-  @Override
-  public void keyPressed(KeyEvent e) {
-    char input = Character.toUpperCase(e.getKeyChar());
+  private void handleKeyPressed(char input) {
     char currentChar = Character.toUpperCase(currentWord.charAt(0));
 
     // If the game is over or the input is not a valid key, return
@@ -206,6 +184,49 @@ public class TypingGame implements KeyListener {
       currentWord = originalWord;
       wordLabel.setText(currentWord);
     }
+  }
+
+  private void setTimeLabel(int time) {
+    timeLabel.setText("Time: " + time + "s");
+  }
+
+  private void newRound() {
+    roundCount++;
+    roundLabel.setText("Round: " + roundCount);
+
+    int randomIndex = (int) (Math.random() * WORDS.length);
+    originalWord = WORDS[randomIndex];
+    currentWord = originalWord;
+    wordLabel.setText(currentWord);
+    wordLabel.setBackground(DEFAULT_MAIN_PANEL_COLOR);
+
+    frame.requestFocusInWindow();
+  }
+
+  private void endGame() {
+    isGameOver = true;
+    timer.stop();
+    // Use HTML to create a new line
+    wordLabel
+        .setText("<html><div style='text-align: center;font-size: 16px;'>Game End!<br>Time used: " + counter + " second"
+            + (counter > 1 ? "s" : "") + "</div></html>");
+  }
+
+  @Override
+  public void keyTyped(KeyEvent e) {
+    // Nothing to do here
+  }
+
+  @Override
+  public void keyReleased(KeyEvent e) {
+    char released = Character.toUpperCase(e.getKeyChar());
+    handleKeyReleased(released);
+  }
+
+  @Override
+  public void keyPressed(KeyEvent e) {
+    char input = Character.toUpperCase(e.getKeyChar());
+    handleKeyPressed(input);
   }
 
   public static void main(String[] args) {
